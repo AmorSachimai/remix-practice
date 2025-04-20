1600f60a8f202583ad3d6ef1b5352f635a3ba76e
幸舞アモル <57258714+AmorSachimai@users.noreply.github.com>
2025-04-20 18:21:51 +0900

    UseCasesのベース部分を作成 (#5)
    * UseCaseのベースを追加
    
    * BookRepositoryを追加
    
    * Entityをエクスポート
    
    * getBookById()を追加
    
    * UseCasesのREADMEファイルをリネーム

---
+++ core/entities/src/index.ts
@@ -0,0 +1,7 @@
+export { BookEntity, type BookRepository } from "./book/entity";
+export type {
+  Book,
+  BookObject,
+  InitBookProps,
+  UpdateBookProps,
+} from "./book/types";
---
+++ core/use-cases/src/book/use-cases.ts
@@ -0,0 +1,13 @@
+import { BookObject, BookRepository } from "@core/entities";
+
+export class BookUseCases {
+  constructor(private repository: BookRepository) {}
+
+  async getBookById(id: string): Promise<BookObject | undefined> {
+    const book = await this.repository.findById(id);
+    if (!book) {
+      return undefined;
+    }
+    return book.freeze();
+  }
+}
