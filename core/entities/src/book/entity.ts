c8c1935cceb1026130aeff8eccbcd552969e100d
amor <yuya.china4869@gmail.com>
2025-04-20 17:13:51 +0900

    BookRepositoryを追加

--- core/entities/src/book/entity.ts
+++ core/entities/src/book/entity.ts
@@ -2,6 +2,13 @@ import { v4 as uuidv4 } from "uuid";
 import { BaseEntity } from "../base-entity";
 import type { Book } from "./types";
 
+export interface BookRepository {
+  findById(id: string): Promise<BookEntity | undefined>;
+  findAll(volume?: number): Promise<BookEntity[]>;
+  findByTitle(title: string): Promise<BookEntity | undefined>;
+  findByISBN(isbn: string): Promise<BookEntity | undefined>;
+}
+
 // ToDo: 作成しながら拡張していく。
 export class BookEntity extends BaseEntity<Book> {
   /**
