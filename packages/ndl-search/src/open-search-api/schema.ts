28151f45ff73d1dfb1490a4bd4a40305f62bd5a1
amor <yuya.china4869@gmail.com>
2025-04-20 10:41:47 +0900

    バリデーション用のRSSスキーマを拡充

--- packages/ndl-search/src/open-search-api/schema.ts
+++ packages/ndl-search/src/open-search-api/schema.ts
@@ -1,46 +1,75 @@
 import { z } from "zod";
 
-const openSearchDcIdentifierSchema = z.object({
+const identifierSchema = z.object({
   "#text": z.union([z.string(), z.number()]),
   "@_xsi:type": z.string(),
 });
 
-const openSearchRdfSeeAlsoSchema = z.object({
-  "@_rdf:resource": z.string(),
-});
+const subjectSchema = z.union([
+  z.string(),
+  z.object({
+    "#text": z.union([z.string(), z.number()]),
+    "@_xsi:type": z.string(),
+  }),
+]);
 
-const openSearchItemSchema = z.object({
+const itemSchema = z.object({
   title: z.string(),
   link: z.string(),
   description: z.string(),
-  author: z.string(),
+  author: z.string().optional(),
   category: z.union([z.string(), z.array(z.string())]),
   "dc:title": z.string(),
-  "dc:creator": z.union([z.string(), z.array(z.string())]).optional(),
-  "dcndl:volume": z.number().optional(),
-  "dc:publisher": z.union([z.string(), z.array(z.string())]).optional(),
-  "dc:identifier": z
-    .union([
-      openSearchDcIdentifierSchema,
-      z.array(openSearchDcIdentifierSchema),
-    ])
+  "dc:date": z
+    .object({
+      "#text": z.union([z.string(), z.number()]),
+      "@_xsi:type": z.string(),
+    })
     .optional(),
-  "rdfs:seeAlso": z
-    .union([openSearchRdfSeeAlsoSchema, z.array(openSearchRdfSeeAlsoSchema)])
+  "dc:description": z.union([
+    z.string(),
+    z.number(),
+    z.array(z.union([z.string(), z.number()])),
+  ]),
+  "dc:extent": z.union([z.string(), z.number()]).optional(),
+  "dc:identifier": z
+    .union([identifierSchema, z.array(identifierSchema)])
+    .optional(),
+  "dc:publisher": z.union([z.string(), z.array(z.string())]).optional(),
+  "dc:subject": z.union([subjectSchema, z.array(subjectSchema)]).optional(),
+  "dcndl:genre": z.string().optional(),
+  "dcndl:genreTranscription": z.string().optional(),
+  "dcndl:price": z.union([z.string(), z.number()]).optional(),
+  "dcndl:publicationPlace": z.string().optional(),
+  "dcndl:seriesTitle": z.string().optional(),
+  "dcndl:titleTranscription": z.string().optional(),
+  "dcndl:volume": z.union([z.string(), z.number()]).optional(),
+  guid: z.object({
+    "#text": z.string(),
+    "@_isPermaLink": z.boolean(),
+  }),
+  pubDate: z.string(),
+  "dc:creator": z.union([z.string(), z.array(z.string())]).optional(),
+  "dcndl:creatorTranscription": z
+    .union([z.string(), z.array(z.string())])
     .optional(),
 });
 
+const channelSchema = z.object({
+  title: z.string(),
+  item: z.union([itemSchema, z.array(itemSchema)]).optional(),
+  description: z.string(),
+  language: z.string(),
+  link: z.string(),
+  "openSearch:itemsPerPage": z.number(),
+  "openSearch:startIndex": z.number(),
+  "openSearch:totalResults": z.number(),
+});
+
+const rssSchema = z.object({
+  channel: channelSchema,
+});
+
 export const openSearchResponseSchema = z.object({
-  rss: z.object({
-    channel: z.object({
-      title: z.string(),
-      item: z.union([openSearchItemSchema, z.array(openSearchItemSchema)]),
-      description: z.string(),
-      language: z.string(),
-      link: z.string(),
-      "openSearch:totalResults": z.number(),
-      "openSearch:startIndex": z.number(),
-      "openSearch:itemsPerPage": z.number(),
-    }),
-  }),
+  rss: rssSchema,
 });
