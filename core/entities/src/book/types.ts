9b207d283c1dbfb97fc8b1af2f9461b9dd4bda24
幸舞アモル <57258714+AmorSachimai@users.noreply.github.com>
2025-05-11 16:53:11 +0900

    BookEntityロジックの詳細を追加 (#7)
    * add isbn3
    
    * add book props type
    
    * fix book logic
    
    * fix mock book repository

--- core/entities/src/book/types.ts
+++ core/entities/src/book/types.ts
@@ -1,10 +1,21 @@
 import type { BaseEntityProps, BaseObject } from "../base-entity";
 
+export type Category = {
+  id: number;
+  name: string;
+  label?: string;
+};
+export type Author = {
+  id: number;
+  name: string;
+  kana: string;
+};
 export type Book = {
+  isbn: string;
   title: string;
   description: string;
-  author: string[];
-  category: string[];
+  authors: Author[];
+  categories: Category[];
 };
 export type BookObject = Readonly<BaseObject<Book>>;
 export type InitBookProps = BaseEntityProps<Book>;
