2adbf2b7ee37bf0c33bd6c6dfb449389e91da3e5
amor <yuya.china4869@gmail.com>
2025-05-17 23:14:35 +0900

    storybookを追加

---
+++ packages/react-ui/src/book-card/index.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { BookCard } from ".";
+
+const meta = {
+  component: BookCard,
+  parameters: {
+    layout: "fullscreen",
+  },
+} satisfies Meta<typeof BookCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Normal: Story = {
+  args: {
+    title: "転生して田舎でスローライフをおくりたい13",
+    imageUrl: "https://ndlsearch.ndl.go.jp/thumbnail/9784299065421.jpg",
+    description:
+      "待望の第13巻は、お米を求めて海向こうの国へと旅立つ、アルフリート一行のストーリー!",
+  },
+};
