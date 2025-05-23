09c596f9b26fe7b5cd4dd56bde93fe5d8f7ffda3
amor <yuya.china4869@gmail.com>
2025-05-24 00:27:11 +0900

    react-uiにカラースキーマを適用

--- packages/react-ui/.storybook/preview.ts
+++ packages/react-ui/.storybook/preview.ts
@@ -1,5 +1,9 @@
 import type { Preview } from "@storybook/react";
 
+// reset.cssをstorybookに適用
+import "@my-packages/style-schema/reset.css";
+import "@my-packages/style-schema/global.css";
+
 const preview: Preview = {
   parameters: {
     controls: {
