3bf6c176c49017f24ad2de1292228d576a0513ac
amor <57258714+AmorSachimai@users.noreply.github.com>
2025-07-30 23:18:44 +0900

    servicesパッケージの追加

---
+++ core/services/src/index.ts
@@ -0,0 +1,2 @@
+export type { BookObject } from "@core/entities";
+export { LibraryService } from "./library/service";
---
+++ core/services/src/library/service.ts
@@ -0,0 +1,42 @@
+import type { BookObject, BookRepository } from "@core/entities";
+import { isISBN } from "@my-packages/utils";
+
+export class LibraryService {
+  constructor(private bookRepo: BookRepository) {}
+
+  /**
+   * 書籍の詳細情報を取得
+   * @param userId ログインユーザーID
+   * @param bookId 書籍ID
+   * @returns
+   */
+  async findBookById(bookId: string): Promise<BookObject | undefined> {
+    const book = await this.bookRepo.findById(bookId);
+    return book?.freeze();
+  }
+
+  /**
+   * 書籍一覧を取得する
+   * @param volume 取得する書籍の数(default: 20)
+   * @param offset 取得する書籍のオフセット(default: 0)
+   * @returns 書籍一覧
+   */
+  async findAll(volume = 20, offset = 0): Promise<BookObject[]> {
+    const books = await this.bookRepo.findAll(volume, offset);
+    return books.map((book) => book.freeze());
+  }
+
+  /**
+   * ISBNから書籍を取得する
+   * @param isbn 書籍のISBN
+   * @returns 書籍情報
+   */
+  async findBookByISBN(isbn: string): Promise<BookObject | undefined> {
+    if (!isISBN(isbn)) {
+      return undefined;
+    }
+
+    const book = await this.bookRepo.findByISBN(isbn);
+    return book?.freeze();
+  }
+}
