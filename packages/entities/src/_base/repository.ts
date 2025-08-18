a418424c4416cf0671c412ee1772c20a57236404
amor <57258714+AmorSachimai@users.noreply.github.com>
2025-08-18 23:51:59 +0900

    BaseRepositoryの修正

--- packages/entities/src/_base/repository.ts
+++ packages/entities/src/_base/repository.ts
@@ -3,5 +3,5 @@ export interface Repository<T> {
   update(entity: T): Promise<void>;
   remove(id: string): Promise<void>;
   findById(id: string): Promise<T | undefined>;
-  findAll(volume?: number, offset?: number): Promise<T[]>;
+  findAll(volume: number, offset: number): Promise<T[]>;
 }
